e83330dddcedae344703d1e909d32d5bc208d047
Leyla Farazha <leyla@lindenlab.com>
2011-04-06 16:21:26 -0700

    updating autobuild.xml and build-cmd.sh directory structure to match new VS2010 structure

--- build-cmd.sh
+++ build-cmd.sh
@@ -21,31 +21,31 @@ set -x
 top="$(pwd)"
 case "$AUTOBUILD_PLATFORM" in
     "windows")
-        build_sln "glodlib.sln" "Debug"
-        build_sln "glodlib.sln" "Release"
-		mkdir -p stage/libraries/i686-win32/lib/{debug,release}
+        build_sln "glodlib.sln" "Debug|Default"
+        build_sln "glodlib.sln" "Release|Default"
+		mkdir -p stage/lib/{debug,release}
         cp "lib/debug/glod.lib" \
-            "stage/libraries/i686-win32/lib/debug/glod.lib"
+            "stage/lib/debug/glod.lib"
         cp "lib/debug/glod.dll" \
-            "stage/libraries/i686-win32/lib/debug/glod.dll"
+            "stage/lib/debug/glod.dll"
         cp "src/api/debug/glod.pdb" \
-            "stage/libraries/i686-win32/lib/debug/glod.pdb"
+            "stage/lib/debug/glod.pdb"
         cp "lib/release/glod.lib" \
-            "stage/libraries/i686-win32/lib/release/glod.lib"
+            "stage/lib/release/glod.lib"
         cp "lib/release/glod.dll" \
-            "stage/libraries/i686-win32/lib/release/glod.dll"
+            "stage/lib/release/glod.dll"
     ;;
         "darwin")
-			libdir="$top/stage/libraries/universal-darwin/"
-            mkdir -p "$libdir"/lib_{debug,release}
+			libdir="$top/stage/lib/"
+            mkdir -p "$libdir"/{debug,release}
 			make -C src clean
 			make -C src debug
 			cp "lib/libGLOD.so" \
-				"$libdir/lib_debug/libglod.so"
+				"$libdir/debug/libglod.so"
 			make -C src clean
 			make -C src release
 			cp "lib/release/libGLOD.so" \
-				"$libdir/lib_release/libglod.so"
+				"$libdir/release/libglod.so"
 		;;
         "linux")
 			libdir="$top/stage/libraries/i686-linux/"
@@ -60,8 +60,8 @@ case "$AUTOBUILD_PLATFORM" in
 				"$libdir/lib_release_client/libglod.so"
         ;;
 esac
-mkdir -p "stage/libraries/include/glod"
-cp "include/glod.h" "stage/libraries/include/glod/glod.h"
+mkdir -p "stage/include/glod"
+cp "include/glod.h" "stage/include/glod/glod.h"
 mkdir -p stage/LICENSES
 cp LICENSE stage/LICENSES/GLOD.txt
 
